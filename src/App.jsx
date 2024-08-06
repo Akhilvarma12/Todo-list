29ed33942d4ba7fb1789313a94c21e72512bc282
Akhilvarma12 <akhilvarma2004@gmail.com>
2024-08-06 23:23:38 +0530

    added css to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import TodoItem from './components/TodoItem'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    ne pellam
+    <div className='todo-container'>
+    <Header/>
+    <TodoItem/>
+    <TodoItem/>
+    <TodoItem/>
+    <TodoItem/>
+    <TodoItem/>
+    <Button/>
+</div>
     </>
   )
 }
